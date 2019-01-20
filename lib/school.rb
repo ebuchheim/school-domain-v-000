5a5c4409f1efae719a8a4b81c286d5a8034990d7
ebuchheim <emma.buchheim@gmail.com>
2019-01-19 18:47:05 -0800

    add class attributes

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+
+    attr_reader :roster, :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if !@roster.keys.include?(grade)
+            @roster[grade] = [name]
+        else 
+            index = @roster[grade].count
+            @roster[grade][index] = name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_roster = {}
+        @roster.each do |grade, students|
+            sorted_roster[grade] = students.sort
+        end
+        sorted_roster
+    end
+end
